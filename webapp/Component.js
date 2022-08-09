a1fe6cf76f88ab2c94b431a7ef7d8c5bf811b3c8
akhilSocius <akhilmohan@sociusus.com>
2022-08-09 08:56:40 +0000

    new

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("list.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
